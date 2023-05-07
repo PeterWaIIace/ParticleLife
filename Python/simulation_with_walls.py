d5b4d411ff7498957173f0fcb4d0f1546c42bfe4
W4ltz <Walas.piotr.prywatny@gmail.com>
2023-05-07 01:18:39 +0100

    update

--- Python/simulation_with_walls.py
+++ Python/simulation_with_walls.py
@@ -3,94 +3,132 @@ import pygame
 import numpy as np
 import time 
 
-maze = [["w","w","w","w","w"],
+
+class MazeWorld:
+
+    def __init__(self):
+        self.screenDim = 400
+        self.maze = [["w","w","w","w","w"],
         ["w","w","w","w","w"],
-        ["w","s","x","x","w"],
+        ["w","s","x","g","w"],
         ["w","w","w","w","w"],
         ["w","w","w","w","w"]]
 
-if __name__=="__main__":
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.screenDim, self.screenDim))
 
-    screenDim = 400
+        Beta = 0.01
+        friction = 0.04
+        rMax = 0.5 # max distance
+        dt = 0.05
+        nColors = 6
 
-    pygame.init()
-    screen = pygame.display.set_mode((screenDim, screenDim))
+        square_height = self.screenDim/len(self.maze)
+        square_width  = self.screenDim/len(self.maze[0])
 
-    Beta = 0.01
-    friction = 0.04
-    rMax = 0.5 # max distance
-    dt = 0.05
-    nColors = 6
+        self.square_height = square_height/self.screenDim
+        self.square_width  = square_width /self.screenDim
 
-    square_height = screenDim/len(maze)
-    square_width  = screenDim/len(maze[0])
+        start_x = 0
+        start_y = 0
 
-    square_height = square_height/screenDim
-    square_width  = square_width/screenDim
+        self.goal_x = 0
+        self.goal_y = 0
+        self.score = 9999999999999999999
 
-    start_x = 0
-    start_y = 0
-
-    for x,squares in enumerate(maze):
-        for y,square in enumerate(squares):
-            if square == "s":
-                start_x = x*square_width + square_width/2
-                start_y = y*square_height+ square_height/2
-
-    particleSystem = ParticleSystem(rMax=rMax,dt=dt,friction=friction,Beta=Beta)
-    particleSystem.loadSystem(start_x * screenDim, start_y * screenDim,screenDim,"particleRobot.json")
-
-    while True:
-        screen.fill((0,0,0))
-
-        # draw maze
-        for x,squares in enumerate(maze):
+        for x,squares in enumerate(self.maze):
             for y,square in enumerate(squares):
-                square_x = x*square_width
-                square_y = y*square_height
+                if square == "s":
+                    start_x = x*self.square_width + self.square_width/2
+                    start_y = y*self.square_height+ self.square_height/2
+                if square == "g":
+                    self.goal_x = x*self.square_width + self.square_width/2
+                    self.goal_y = y*self.square_height+ self.square_height/2
+
+        self.particleSystem = ParticleSystem(rMax=rMax,dt=dt,friction=friction,Beta=Beta)
+        self.particleSystem.loadSystem(start_x * self.screenDim, start_y * self.screenDim,self.screenDim,"particleRobot.json")
+
+    def __drawMaze(self):
+        for x,squares in enumerate(self.maze):
+            for y,square in enumerate(squares):
+                square_x = x*self.square_width
+                square_y = y*self.square_height
                 if square == "w":
-                    pygame.draw.rect(screen, (40,0,40), pygame.Rect(square_x * screenDim, square_y * screenDim, square_width * screenDim, square_height * screenDim))
+                    pygame.draw.rect(self.screen, (40,0,40), pygame.Rect(square_x * self.screenDim, square_y * self.screenDim, self.square_width * self.screenDim, self.square_height * self.screenDim))
                 elif square == "s":
-                    pygame.draw.rect(screen, (10,10,0), pygame.Rect(square_x * screenDim, square_y * screenDim, square_width * screenDim, square_height * screenDim))
+                    pygame.draw.rect(self.screen, (10,10,0), pygame.Rect(square_x * self.screenDim, square_y * self.screenDim, self.square_width * self.screenDim, self.square_height * self.screenDim))
                 elif square == "x":
-                    pygame.draw.rect(screen, (0,0,0),   pygame.Rect(square_x * screenDim, square_y * screenDim, square_width * screenDim, square_height * screenDim))
+                    pygame.draw.rect(self.screen, (0,0,0),   pygame.Rect(square_x * self.screenDim, square_y * self.screenDim, self.square_width * self.screenDim, self.square_height * self.screenDim))
+                elif square == "g":
+                    pygame.draw.rect(self.screen, (35,35,0),   pygame.Rect(square_x * self.screenDim, square_y * self.screenDim, self.square_width * self.screenDim, self.square_height * self.screenDim))
 
-        particleSystem.updateVelocities()
-
+    def __collisions(self):
         # check collisions with maze
-        for n,particle in enumerate(particleSystem.positions):
-            for x,squares in enumerate(maze):
+        for n,particle in enumerate(self.particleSystem.positions):
+            for x,squares in enumerate(self.maze):
                 for y,square in enumerate(squares):
-                    square_y = y * square_height
-                    square_x = x * square_width
+                    square_y = y * self.square_height
+                    square_x = x * self.square_width
                     if square == "w":
                         pos_x = particle[0]
                         pos_y = particle[1]
 
-                        if pos_x > square_x and pos_x < square_x + square_width and pos_y > square_y and pos_y < square_y + square_height:
+                        if pos_x > square_x and pos_x < square_x + self.square_width and pos_y > square_y and pos_y < square_y + self.square_height:
 
-                            particleSystem.velocities[n][0] = 0
-                            particleSystem.velocities[n][1] = 0
+                            self.particleSystem.velocities[n][0] = 0
+                            self.particleSystem.velocities[n][1] = 0
 
-                            if abs(pos_x - square_x) > abs(pos_x - (square_x + square_width)):
-                                particleSystem.positions[n][0] += 0.001
-                            elif abs(pos_x - square_x) < abs(pos_x - (square_x + square_width)):
-                                particleSystem.positions[n][0] -= 0.001
+                            if abs(pos_x - square_x) > abs(pos_x - (square_x + self.square_width)):
+                                self.particleSystem.positions[n][0] += 0.001
+                            elif abs(pos_x - square_x) < abs(pos_x - (square_x + self.square_width)):
+                                self.particleSystem.positions[n][0] -= 0.001
 
-                            if abs(pos_y - square_y) > abs(pos_y - (square_y + square_height)):
-                                particleSystem.positions[n][1] += 0.001
-                            elif abs(pos_y - square_y) < abs(pos_y - (square_y + square_height)):
-                                particleSystem.positions[n][1] -= 0.001
+                            if abs(pos_y - square_y) > abs(pos_y - (square_y + self.square_height)):
+                                self.particleSystem.positions[n][1] += 0.001
+                            elif abs(pos_y - square_y) < abs(pos_y - (square_y + self.square_height)):
+                                self.particleSystem.positions[n][1] -= 0.001
 
-                            # pygame.draw.rect(screen, (50,0,50), pygame.Rect(square_x * screenDim, square_y * screenDim, square_width * screenDim, square_height * screenDim))
+                            # pygame.draw.rect(self.screen, (50,0,50), pygame.Rect(square_x * self.screenDim, square_y * self.screenDim, square_width * self.screenDim, square_height * self.screenDim))
 
-        particleSystem.updatePositions()
-        drawParticles(screen=screen,nParticles=particleSystem.nParticles,colors=particleSystem.colors,positions=particleSystem.positions,screenDim=screenDim)
+    def __score(self):
+        self.score = 0.0
+        for particle in self.particleSystem.positions:
+            self.score += np.linalg.norm(np.array([self.goal_x,self.goal_y]) - particle)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+        self.score /= len(self.particleSystem.positions)
 
-        pygame.display.update()
+    def silentLoop(self):
+        for _ in range(100):
+            self.particleSystem.updateVelocities()
+            self.__collisions()
+            self.particleSystem.updatePositions()
 
-    pygame.quit()
+        self.__score()
+
+    def loop(self):
+        for _ in range(10000):
+            self.screen.fill((0,0,0))
+
+            # draw maze
+            self.__drawMaze()
+            self.particleSystem.updateVelocities()
+            self.__collisions()
+            self.particleSystem.updatePositions()
+
+            drawParticles(screen=self.screen,nParticles=self.particleSystem.nParticles,colors=self.particleSystem.colors,positions=self.particleSystem.positions,screenDim=self.screenDim)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+            pygame.display.update()
+
+        self.__score()
+        print(self.score)
+        pygame.quit()
+
+
+if __name__=="__main__":
+
+    mw = MazeWorld()
+    mw.loop()
