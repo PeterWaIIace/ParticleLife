24b73ce0034cb81b537631456c549586229695da
W4ltz <Walas.piotr.prywatny@gmail.com>
2023-07-25 00:01:04 +0100

    One second faster than legacy with buckets

--- Cpp/particleLab/particle.cpp
+++ Cpp/particleLab/particle.cpp
@@ -8,12 +8,44 @@
 #include <iostream>
 using namespace std::chrono;
 
+double F(float distance,float a, float b)
+{
+    if(distance < b)
+    {
+        return distance/b - 1;
+    }
+    else if(b < distance && distance < 1.0)
+    {
+        return a * (1 - fabs(2.0 * distance - 1.0 - b)/(1 - b));
+    }
+    else
+    {
+        return 0.0;
+    }
+}
+
 class Particle{
     // MOCK
+
+    double x, y;
+    double xDiff,yDiff;
+    double f_x ,f_y;
     public:
         void interact(Particle& other){
-            Sleep(0.1);
+            double relation = 1.0;
+            xDiff  = other.x - x;
+            yDiff  = other.y - y;
+
+            float r = sqrt(pow(other.x - x,2) + pow(other.y - y,2));
+
+            if(0 < r && r < 10)
+            {
+                float f = F(r/10,relation,0.3);
+                f_x += (other.x - x)/r * 50.0;
+                f_y += (other.y - y)/r * 50.0;
+            }
         };
+
 };
 
 class ParticleSystem
