0d47405bbaba5bd46888e34b2b2bca163e29da09
W4ltz <Walas.piotr.prywatny@gmail.com>
2023-07-25 01:09:19 +0100

    random unifrom distribution added to particles generation

--- Cpp/particleLab/particle.cpp
+++ Cpp/particleLab/particle.cpp
@@ -6,6 +6,8 @@
 #include <windows.h>
 #include <chrono>
 #include <iostream>
+#include <random>
+
 using namespace std::chrono;
 
 double F(float distance,float a, float b)
@@ -28,23 +30,52 @@ class Particle{
     // MOCK
 
     double x, y;
-    double xDiff,yDiff;
+    double v_x = 0.0, v_y = 0.0;
     double f_x ,f_y;
+    double dt = 0.1;
+    double friction = 0.01;
+    double rMax = 10.0;
+    double force = 50.0;
+    double beta = 0.3;
+
     public:
+        Particle(double x, double y)
+        {
+            this->x = x;
+            this->y = y;
+        }
+
         void interact(Particle& other){
             double relation = 1.0;
-            xDiff  = other.x - x;
-            yDiff  = other.y - y;
 
             float r = sqrt(pow(other.x - x,2) + pow(other.y - y,2));
 
             if(0 < r && r < 10)
             {
-                float f = F(r/10,relation,0.3);
+                float f = F(r/rMax,relation,0.3);
                 f_x += (other.x - x)/r * 50.0;
                 f_y += (other.y - y)/r * 50.0;
             }
         };
+
+        void Particle::updateVelocity()
+        {
+            f_x *= rMax * force;
+            f_y *= rMax * force;
+
+            v_x *= friction;
+            v_y *= friction;
+
+            v_x += f_x * dt;
+            v_y += f_y * dt;
+        }
+
+
+        void Particle::updatePostion()
+        {
+            x += v_x * dt;
+            y += v_y * dt;
+        }
 
 };
 
@@ -57,9 +88,15 @@ class ParticleSystem
     public:
         void init(int initSize = 10000)
         {
+            double lower_bound = 0.0;
+            double upper_bound = 1.0;
+            std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
+            std::default_random_engine re;
+
             for(int i = 0 ; i < initSize ; i++)
             {
-                frame.push_back(Particle());
+
+                frame.push_back(Particle(unif(re),unif(re)));
             }
         }
 
@@ -97,7 +134,7 @@ void timeit(std::function<void(void)> fn)
 int main(int argc, char* argv[])
 {
     ParticleSystem system;
-    system.init(10000);
+    system.init(1000);
     timeit([&system](){
         system.run();
     });
